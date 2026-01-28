488c0b74d2fa5c2ecea7bcc0f7e71827558bd655
Pratham <chudasamapratham142@gmail.com>
2026-01-28 10:20:33 +0530

    feat: integrate all interactive components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Carousel from "./components/Carousel"
+import Carousel from './components/Carousel';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Star from './components/Star';
+import ThreeD from './components/3d';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
-    <main className="w-full h-screen bg-black">
+    <main className="w-full min-h-screen bg-[#FAF3E1] transition-colors duration-300">
       <Carousel />
+      <Star />
+      <ThreeD />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
